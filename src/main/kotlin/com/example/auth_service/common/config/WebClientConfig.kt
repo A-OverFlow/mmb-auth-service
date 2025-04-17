7bf077b6e23d63a32899bec1fa353f94182e45af
tear94fall <tear94fall@naver.com>
2025-04-17 22:31:48 +0900

    Feat: "회원 가입 API 수정"
    회원 가입 API 수정
    
    Resolves: N/A
    Ref: N/A

--- src/main/kotlin/com/example/auth_service/common/config/WebClientConfig.kt
+++ src/main/kotlin/com/example/auth_service/common/config/WebClientConfig.kt
@@ -1,11 +1,24 @@
 package com.example.auth_service.common.config
 
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.web.reactive.function.client.WebClient
 
 @Configuration
 class WebClientConfig {
+
+    @Value("\${common.service.member.host}")
+    private lateinit var host: String
+
+    @Value("\${common.service.member.port}")
+    private var port: Int = 0
+
     @Bean
-    fun webClient(): WebClient = WebClient.builder().build()
+    fun webClient(): WebClient {
+        val baseUrl = "http://$host:$port"
+        return WebClient.builder()
+            .baseUrl(baseUrl)
+            .build()
+    }
 }
